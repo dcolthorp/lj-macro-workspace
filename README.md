bdf0d4ddef9f6591b4bad7c86c2da6d02dae8528
Micah Alles <micah@atomicobject.com>
2013-07-09 21:46:15 -0500

    commented out tests, updated README

--- README.md
+++ README.md
@@ -1,21 +1,27 @@
 # macro-workshop-playground
 
-Welcome to the macro workshop for LambdaJam. Install leiningen (http://leiningen.org/) and clone this repository.
+Welcome to the LambdaJam macro workshop. Install leiningen (http://leiningen.org/) and clone this repository.
 
-If everything's working properly, you should be able to run
+If everything's working properly, you should be able to run:
 
-lein run
+    lein test
 
-at your terminal within this repository and see a welcome message.
+at your terminal within this repo and see the following output:
 
+    lein test user
 
-## Usage
+    Ran 0 tests containing 0 assertions.
+    0 failures, 0 errors.
 
-lein run
+## Exercises
 
-or
+Empty function and macro shells for the exercises can be found in `src/macro_workshop_playground/core.clj` and commented out test cases for each can be found in `test/macro_workshop_playground/core_test.clj`
 
-lein repl
+If you're new to Clojure development and/or leinengen open two terminals and in one run `lein repl` and save the other for running `lein test`.
+
+To test-drive your work run uncomment a test case(s) for an exercise in `test/macro_workshop_playground/core_test.clj` and run `lein test` to see it fail, update the corresponding function or macro shell in `src/macro_workshop_playground/core.clj` and run `lein test` again to see if it passes.
+
+You should also use the repl to experiment with Clojure functions and your macros - using macroexpand-1, for example. If you want to reload your source file from the repl call `(use :reload-all 'macro-workshop-playground.core)` or type `quit` and `lein repl` to restart it.
 
 ## License
 
--- test/macro_workshop_playground/core_test.clj
+++ test/macro_workshop_playground/core_test.clj
@@ -10,31 +10,31 @@
  
 ;; 1. funhouse
 
-(deftest funhouse-test
-  (testing "funhouse"
-    (is (= (funhouse '(+ 3 1)) '(- 3 1)))
-    (is (= (funhouse '(- 3 1)) '(+ 3 1)))
-    (is (= (funhouse '(* 3 1)) '(/ 3 1)))
-    (is (= (funhouse '(/ 3 1)) '(* 3 1)))))
+; (deftest funhouse-test
+;   (testing "funhouse"
+;     (is (= (funhouse '(+ 3 1)) '(- 3 1)))
+;     (is (= (funhouse '(- 3 1)) '(+ 3 1)))
+;     (is (= (funhouse '(* 3 1)) '(/ 3 1)))
+;     (is (= (funhouse '(/ 3 1)) '(* 3 1)))))
 
 ;; 2. Recursive funhouse
 
-(deftest recursive-funhouse-test
-  (testing "recursive funhouse"
-    (is (= (funhouse '(+ 1 (/ 3 1))) '(- 1 (* 3 1))))))
+; (deftest recursive-funhouse-test
+;   (testing "recursive funhouse"
+;     (is (= (funhouse '(+ 1 (/ 3 1))) '(- 1 (* 3 1))))))
 
 ;; 3. Extra credit
 
-(deftest extra-credit-funhouse-test
-  (testing "funhouse recursing into maps, vectors, and sets"
-    (is (= (funhouse '(+ (/ 2 2)
-                         (first #{(- 1 1)})
-                         (first [(- 1 1)])
-                         (:2nd {:1st 1, :2nd (+ 5 1)})))
-           '(- (* 2 2)
-               (first #{(+ 1 1)})
-               (first [(+ 1 1)])
-               (:2nd {:1st 1, :2nd (- 5 1)}))))))
+; (deftest extra-credit-funhouse-test
+;   (testing "funhouse recursing into maps, vectors, and sets"
+;     (is (= (funhouse '(+ (/ 2 2)
+;                          (first #{(- 1 1)})
+;                          (first [(- 1 1)])
+;                          (:2nd {:1st 1, :2nd (+ 5 1)})))
+;            '(- (* 2 2)
+;                (first #{(+ 1 1)})
+;                (first [(+ 1 1)])
+;                (:2nd {:1st 1, :2nd (- 5 1)}))))))
 
 ;;;;
 ;;;; Macros
@@ -42,17 +42,17 @@
  
 ;; 1. in-funhouse
 
-(deftest in-funhouse-test
-  (testing "in-funhouse"
-    (is (= (in-funhouse (+ 6 1)) 5))))
+; (deftest in-funhouse-test
+;   (testing "in-funhouse"
+;     (is (= (in-funhouse (+ 6 1)) 5))))
 
 ;; 2. do-funhouse
 
-(deftest do-funhouse-test
-  (testing "do-funhouse"
-    (with-local-vars [v nil]
-      (is (= 1 (do-funhouse (var-set v (+ 1 2)) (* 2 2))))
-      (is (= -1 @v)))))
+; (deftest do-funhouse-test
+;   (testing "do-funhouse"
+;     (with-local-vars [v nil]
+;       (is (= 1 (do-funhouse (var-set v (+ 1 2)) (* 2 2))))
+;       (is (= -1 @v)))))
 
 ;;;;
 ;;;; Syntax-quote
@@ -60,16 +60,16 @@
 
 ;; 1. rewrite debug using syntax-quote
 
-(deftest debug-test
-  (testing "debug"
-    (is (= 3 (debug (+ 1 2))))))
+; (deftest debug-test
+;   (testing "debug"
+;     (is (= 3 (debug (+ 1 2))))))
 
-(deftest debug-variable-capture-test
-  (testing "debug"
-    (with-local-vars [v nil]
-      (is (= 3 (let [println (fn [& args] (var-set v :busted))]
-                 (debug (+ 1 2)))))
-      (is (= nil @v)))))
+; (deftest debug-variable-capture-test
+;   (testing "debug"
+;     (with-local-vars [v nil]
+;       (is (= 3 (let [println (fn [& args] (var-set v :busted))]
+;                  (debug (+ 1 2)))))
+;       (is (= nil @v)))))
 
 ;;;;
 ;;;; Multiple execution
@@ -77,20 +77,20 @@
 
 ;; 1. fix multiple exection in debug
 
-(deftest debug-single-eval-test
-  (testing "debug only evaluates the expression once"
-    (let [times (atom 0)]
-      (is (= 3 (debug (do (swap! times inc) (+ 1 2)))))
-      (is (= 1 @times)))))
+; (deftest debug-single-eval-test
+;   (testing "debug only evaluates the expression once"
+;     (let [times (atom 0)]
+;       (is (= 3 (debug (do (swap! times inc) (+ 1 2)))))
+;       (is (= 1 @times)))))
 
 ;; 2. and–1
 
-(deftest and-1-test
-  (testing "and-1"
-    (is (= :foo (and-1 true :foo)))
-    (with-local-vars [v nil]
-      (is (= nil (and-1 nil (var-set v "busted"))))
-      (is (= nil @v)))))
+; (deftest and-1-test
+;   (testing "and-1"
+;     (is (= :foo (and-1 true :foo)))
+;     (with-local-vars [v nil]
+;       (is (= nil (and-1 nil (var-set v "busted"))))
+;       (is (= nil @v)))))
 
 ;;;;
 ;;;; Using functions in macros
@@ -98,26 +98,26 @@
 
 ;; 1. stringify-static
 
-(deftest stringify-static-simple
-  (testing "stringify-static"
-    (is (= '(":foo" (- 3 4) "1" (+ 1 2) "jon" bar)
-           (stringify-static '(:foo (- 3 4) 1 (+ 1 2) "jon" bar))))))
+; (deftest stringify-static-simple
+;   (testing "stringify-static"
+;     (is (= '(":foo" (- 3 4) "1" (+ 1 2) "jon" bar)
+;            (stringify-static '(:foo (- 3 4) 1 (+ 1 2) "jon" bar))))))
 
 ;; 2. pre-str
 
-(deftest pre-str-test
-  (testing "pre-str"
-    (let [bar "baz"]
-      (is (= "1 :foo 3 baz"
-             (pre-str 1 " " :foo " " (+ 1 2) " " bar))))))
+; (deftest pre-str-test
+;   (testing "pre-str"
+;     (let [bar "baz"]
+;       (is (= "1 :foo 3 baz"
+;              (pre-str 1 " " :foo " " (+ 1 2) " " bar))))))
 
 ;; 2. Extra credit
 
-(deftest stringify-static-extra-credit
-  (testing "stringify-static collapses static runs"
-    (is (= '(":foo 12jonboy3" (+ 1 2) bar "42")
-           (stringify-static '(:foo " " 1 2 "jon"
-                                    "boy" 3 (+ 1 2) bar 42))))))
+; (deftest stringify-static-extra-credit
+;   (testing "stringify-static collapses static runs"
+;     (is (= '(":foo 12jonboy3" (+ 1 2) bar "42")
+;            (stringify-static '(:foo " " 1 2 "jon"
+;                                     "boy" 3 (+ 1 2) bar 42))))))
 
 ;;;;
 ;;;; Recursive macros
@@ -125,13 +125,13 @@
 
 ;; 1. and-*
 
-(deftest and-*-test
-  (testing "and-*"
-    (is (= true (and-*)))
-    (is (= 0 (and-* 0)))
-    (is (= 1 (and-* 0 1)))
-    (is (= 2 (and-* 0 1 2)))
-    (with-local-vars [v nil]
-      (is (= nil (and-* 0 nil (var-set v "busted"))))
-      (is (= nil @v)))))
+; (deftest and-*-test
+;   (testing "and-*"
+;     (is (= true (and-*)))
+;     (is (= 0 (and-* 0)))
+;     (is (= 1 (and-* 0 1)))
+;     (is (= 2 (and-* 0 1 2)))
+;     (with-local-vars [v nil]
+;       (is (= nil (and-* 0 nil (var-set v "busted"))))
+;       (is (= nil @v)))))
 
