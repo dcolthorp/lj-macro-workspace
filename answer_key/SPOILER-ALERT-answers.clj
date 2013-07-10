5f5e95d75916a667af8fc64dd61359b747f8490f
Micah Alles <micah@atomicobject.com>
2013-07-10 15:05:45 -0500

    added answer key

---
+++ answer_key/SPOILER-ALERT-answers.clj
@@ -0,0 +1,76 @@
+(ns macro-workshop-playground.core
+  (:require clojure.walk)
+  [:use clojure.tools.macro [clojure.walk :as walk]])
+(def ... '())
+
+
+;;;;
+;;;; Funhouse
+;;;;
+
+(defn funhouse [forms] 
+  (postwalk-replace '{+ -, - +, * /, / *} forms))
+
+
+;;;;
+;;;; Macros
+;;;;
+ 
+(defmacro in-funhouse [forms] 
+  (funhouse forms))
+
+(defmacro do-funhouse [& forms] 
+  (cons 'do (map funhouse forms)))
+
+
+;;;;
+;;;; Syntax-quote
+;;;;
+
+(defmacro debug [expr]
+  `(let [x# ~expr]
+     (println 'x# :> x#)
+     x#))
+
+
+;;;;
+;;;; Multiple execution
+;;;;
+
+(defmacro and-1 [expr1 expr2]
+  `(let [x# ~expr1]
+    (if x#
+      ~expr2
+      x#)))
+
+
+;;;;
+;;;; Using functions in macros
+;;;;
+
+(defn stringify-static [parts]
+  (let [static? (some-fn keyword? string? number?)
+        partitioned (partition-by static? parts)]
+    (mapcat (fn [coll]
+              (if (static? (first coll))
+                [(apply str coll)]
+                coll))
+            partitioned)))
+
+(defmacro pre-str [& parts]
+  `(str ~@(stringify-static parts)))
+
+
+;;;;
+;;;; Recursive macros
+;;;;
+
+(defmacro and-*
+  ([] true)
+  ([expr] expr)
+  ([expr & exprs]
+  `(let [x# ~expr]
+    (if x#
+      (and-* ~@exprs)
+      x#))))
+
